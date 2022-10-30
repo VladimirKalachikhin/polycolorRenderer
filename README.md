bd036fae57c7bea9afa8ee431b7fe587fd763b80
Stager <v.kalachikhin@gmail.com>
2022-10-30 11:58:58 +0300

    improved

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # polycolorRenderer [![License: CC BY-SA 4.0](https://img.shields.io/badge/License-CC%20BY--SA%204.0-lightgrey.svg)](https://creativecommons.org/licenses/by-sa/4.0/)
-## v. 0.1.0  
+## v. 0.2
 The Leaflet L.Renderer upon the idea [leaflet-polycolor](https://github.com/Oliv/leaflet-polycolor) project.  
 
-Suitable for color-coding a Leaflet polyline based on additional values, e.g. altitude, speed, depth, etc.. Main goal is a gradient color of segments to represent continuous values.
+Suitable for color-coding a Leaflet polyline based on additional values, e.g. altitude, speed, depth, etc... Main goal is a gradient color of segments to represent continuous values.  
+Additionally, it is possible to set the `weight` option of each L.Polyline segment.
 
-It can be used both for coloring both L.Polyline and L.GeoJSON LineString features.
+It can be used both for L.Polyline and L.GeoJSON LineString features.
 
 ## Usage
 `<script src="polycolorRenderer.js"></script>`  
@@ -23,7 +24,8 @@ As the renderer has a narrow purpose, only the following options of L.Polyline a
 let polyline = L.polyline(latLngs, {
 	renderer: new polycolorRenderer(),
 	colors: [......], // array of html colors
-	useGradient: false
+	useGradient: false,
+	weights: [......] // array of segment's width
 }).addTo(map);
 ```
 
@@ -34,7 +36,8 @@ var polyline = L.geoJSON(geoJSONdata,{
 	style: function(feature){
 		return {
 			renderer: new polycolorRenderer(),
-			colors: [......] // array of html colors
+			colors: [......], // array of html colors
+			weights: [......] // array of segment's width
 		}
 	}
 }).addTo(map);
@@ -50,15 +53,18 @@ const colors = [
 	[]
 ];
 ```
+The same for `weights` array.
 
 ### Options
 
-- colors `<Array>` : An array of html colors strings `rgb(x,y,z), rgba(x,y,z,t)`or '#3388ff', or array of arrays of colors. Any of these arrays can be empty.
-- useGradient `<Boolean>` : Use or not gradient to smooth colors. Defaults to `true`
+- `colors` \<Array>: An array of html colors strings `rgb(x,y,z), rgba(x,y,z,t)`or '#3388ff', or array of arrays of colors. Any of these arrays can be empty.
+- `useGradient` \<Boolean>: Use or not gradient to smooth colors. Defaults to `true. `
+- `weights` \<Array>: An array of segments stroke width in pixels. The number of values of this array is equal to the number of segments, that is a number of colors - 1.
 
-If `colors` are `undefined`, the default `color` parameter is used.
-To leave default color, use `null` in colors:  
-`['rgb(0, 0, 0)', null, 'rgb(0, 45, 120)']`
+If `colors`or weight are `undefined`, the default `color`or weight parameter is used.
+To leave default value, use `null` in array:  
+`['rgb(0, 0, 0)', null, 'rgb(0, 45, 120)']`  
+No gradient is made between the default and the specified values.
 
 ## Demo
 The _examples_ directory has a complete example of using the polycolorRenderer with GeoJSON data.
--- polycolorRenderer.js
+++ polycolorRenderer.js
@@ -1,4 +1,5 @@
 /* Vladimir Kalachikhin v.kalachikhin@gmail.com CC BY-SA
+version 0.2.0
 The Renderer upon the idea https://github.com/Oliv/leaflet-polycolor 
 to colorisation of L.Polyline segments
 Only the following options are supported:
@@ -18,57 +19,74 @@ const polycolorRenderer = L.Canvas.extend({
 _updatePoly: function(layer) {
 
 	if (!this._drawing) return;
+	if (layer.options.weight === 0) return;	// заданы нулевая толщина линии -- нечего рисовать
 
 	let i, j, len2, Point, prevPoint;
 	// Polyline может быть MultiPolyline -- состоять из массива массивов координат
 	// Например, когда она создаётся из GeoJSON MultiLineString.
-	// Если Polyline сделана из одного массива координат, то в _parts -- один массив.
-	if (!layer._parts.length) return;
+	// Если Polyline сделана из одного массива координат, то _parts -- массив из одного массива.
+	if (!layer._parts.length) return;	// нет ни одного сегмента в линни
 	//console.log('polycolorRenderer.js [polycolorRenderer] layer:',layer);
 
 	this._layers[layer._leaflet_id] = layer;	// ?
 	const ctx = this._ctx;
 	
-	// Нормализуем базовый цвет и массив цветов
-	let optColors;
+	// Зададим остальные свойства линии
+	if (ctx.setLineDash) {
+		ctx.setLineDash(layer.options && layer.options._dashArray || []);
+	}
+	ctx.globalAlpha = layer.options.opacity;
+	ctx.lineCap = layer.options.lineCap;
+	ctx.lineJoin = layer.options.lineJoin;
+
+	// Нормализуем массивы цветов и толщин
+	let optColors, optWeights;
 	if((typeof layer.options.colors !== 'undefined') || Array.isArray(layer.options.colors)) {
 		if(!Array.isArray(layer.options.colors[0])) optColors = [layer.options.colors];	// сделаем из options.colors массив массивов, такой же по структуре, как _parts
 		else optColors = layer.options.colors;	// считаем, что options.colors уже такой же по структуре, как _parts
 	}
-	// Покрасим линии
-	if (layer.options.stroke && layer.options.weight !== 0) {	// заданы обводка линии и её толщина
-		if (ctx.setLineDash) {
-			ctx.setLineDash(layer.options && layer.options._dashArray || []);
-		}
-		ctx.globalAlpha = layer.options.opacity;
-		ctx.lineWidth = layer.options.weight;
-		ctx.lineCap = layer.options.lineCap;
-		ctx.lineJoin = layer.options.lineJoin;
+	if((typeof layer.options.weights !== 'undefined') || Array.isArray(layer.options.weights)) {
+		if(!Array.isArray(layer.options.weights[0])) optWeights = [layer.options.weights];
+		else optWeights = layer.options.weights;
+	}
+	
+	// Каждый отрезок каждого сегмента, по всем элементам массива массивов
+	for (i = 0; i < layer._parts.length; i++) {	
+		for (j = 0, len2 = layer._parts[i].length - 1; j < len2; j++) {
+			Point = layer._parts[i][j + 1];
+			prevPoint = layer._parts[i][j];
 
-		for (i = 0; i < layer._parts.length; i++) {	// для каждого куска MultiPolyline 
-			for (j = 0, len2 = layer._parts[i].length - 1; j < len2; j++) {
-				Point = layer._parts[i][j + 1];
-				prevPoint = layer._parts[i][j];
+			ctx.beginPath();
+			ctx.moveTo(prevPoint.x, prevPoint.y);
+			ctx.lineTo(Point.x, Point.y);
 
-				ctx.beginPath();
-				ctx.moveTo(prevPoint.x, prevPoint.y);
-				ctx.lineTo(Point.x, Point.y);
-
-				if(optColors){
-					if(optColors[i].length == 0) ctx.strokeStyle = layer.options.color;	// пустой массив, линию окрашиваем в указанный, или умолчальный цвет (layer.options.color всегда есть?)
-					else if(optColors[i].length < (j+1)) ctx.strokeStyle = optColors[i][0] || layer.options.color;	// одно значение на все сегменты или неверное число значений, +1 потому что градиент
-					else {	// массив значений цветов
-						if(layer.options.useGradient) {
-							ctx.strokeStyle = this._getStrokeGradient(ctx, prevPoint, Point, optColors[i][j] || layer.options.color, optColors[i][j+1] || layer.options.color);	// требуется градиент
+			// Покрасим линии
+			if(optColors){
+				if(optColors[i].length == 0) ctx.strokeStyle = layer.options.color;	// пустой массив, линию окрашиваем в умолчальный цвет (layer.options.color всегда есть?)
+				else if(optColors[i].length < (j+1)) ctx.strokeStyle = optColors[i][0] || layer.options.color;	// одно значение на все сегменты или неверное число значений, +1 потому что градиент
+				else {	// массив значений цветов
+					if(layer.options.useGradient) {	// требуется градиент
+						if(optColors[i][j] && optColors[i][j+1]) {	// рисуем градиент только если есть содержательные значения
+							ctx.strokeStyle = this._getStrokeGradient(ctx, prevPoint, Point, optColors[i][j], optColors[i][j+1]);
 						}
-						else ctx.strokeStyle = optColors[i][j] || layer.options.color;
+						else ctx.strokeStyle = layer.options.color;	// не рисуем градиент к умолчальному значению
 					}
+					else ctx.strokeStyle = optColors[i][j] || layer.options.color;
 				}
-				else ctx.strokeStyle = layer.options.color;
-
-				ctx.stroke();
-				ctx.closePath();
 			}
+			else ctx.strokeStyle = layer.options.color;
+
+			// Зададим толщину
+			if(optWeights){
+				console.log(i,j,optWeights[i][j]);
+				if(optWeights[i].length == 0) ctx.lineWidth = layer.options.weight;	// пустой массив, задаём толщину линнии по умолчанию (layer.options.weight всегда есть?)
+				else if(optWeights[i].length < j) ctx.lineWidth = optWeights[i][0] || layer.options.weight;	// одно значение на все сегменты или неверное число значений
+				else ctx.lineWidth = optWeights[i][j] || layer.options.weight;
+			}
+			else ctx.lineWidth = layer.options.weight;
+
+			ctx.stroke();
+			ctx.closePath();
 		}
 	}
 },
